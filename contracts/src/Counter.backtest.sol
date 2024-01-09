fe34ce8cf5bdf0c876780b87e96d1c44646bafa2
Shelven Zhou <favoring.zhou49@gmail.com>
2024-01-09 15:11:24 +0800

    add Counter backtest example

---
+++ contracts/src/Counter.backtest.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {BaseTestHooks} from "@uniswap/v4-core/src/test/BaseTestHooks.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+
+import {Counter} from "./Counter.sol";
+
+contract CounterBacktest is BaseTestHooks {
+    using PoolIdLibrary for PoolKey;
+
+    Counter public immutable counter;
+
+    constructor(IPoolManager _poolManager) {
+        counter = new Counter(_poolManager);
+    }
+
+    function getHookPermissions() public pure returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: true,
+            afterAddLiquidity: true,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            noOp: false,
+            accessLock: false
+        });
+    }
+
+    function beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata hookData)
+        external
+        override
+        returns (bytes4)
+    {
+        return counter.beforeSwap(sender, key, params, hookData);
+    }
+
+    function afterSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params, BalanceDelta delta, bytes calldata hookData)
+        external
+        override
+        returns (bytes4)
+    {
+        return counter.afterSwap(sender, key, params, delta, hookData);
+    }
+
+    function beforeAddLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata params,
+        bytes calldata hookData
+    ) external override returns (bytes4) {
+        return counter.beforeAddLiquidity(sender, key, params, hookData);
+    }
+
+    function afterAddLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata params,
+        BalanceDelta delta,
+        bytes calldata hookData
+    ) external override returns (bytes4) {
+        return counter.afterAddLiquidity(sender, key, params, delta, hookData);
+    }
+}
